bfa7e3c2d5f4259440fbd58995bb0813ed5cf467
Ian Crane <ian.crane12@gmail.com>
2022-11-16 13:42:47 -0600

    completed review

--- advanced_dataframes.py
+++ advanced_dataframes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[132]:
+# In[7]:
 
 
 import pandas as pd
@@ -14,20 +14,20 @@ def get_db_url(username, hostname, password, database):
     return url
 
 
-# In[195]:
+# In[8]:
 
 
 url = get_db_url(username, hostname, password, 'employees')
 
 
-# In[197]:
+# In[11]:
 
 
 employees = pd.read_sql('SELECT * FROM employees', url)
 employees
 
 
-# In[198]:
+# In[12]:
 
 
 titles = pd.read_sql('SELECT * FROM titles', url)
@@ -46,10 +46,10 @@ employees.info(), employees.describe()
 titles.info(), titles.describe()
 
 
-# In[38]:
+# In[25]:
 
 
-titles.title.unique()
+titles.title.nunique()
 
 
 # In[39]:
@@ -58,16 +58,16 @@ titles.title.unique()
 titles.to_date.max()
 
 
-# In[52]:
+# In[26]:
+
+
+titles.to_date.min()
+
+
+# In[29]:
 
 
 from datetime import date
-titles[titles.to_date < date.today()].sort_values(by='to_date').head(1)
-
-
-# In[53]:
-
-
 titles[titles.to_date < date.today()].sort_values(by='to_date', ascending=False).head(1)
 
 
@@ -77,7 +77,7 @@ titles[titles.to_date < date.today()].sort_values(by='to_date', ascending=False)
 # Exercises II
 
 
-# In[71]:
+# In[31]:
 
 
 # 1. Copy the users and roles DataFrames from the examples above.
@@ -91,7 +91,7 @@ users = pd.DataFrame({
 users
 
 
-# In[72]:
+# In[32]:
 
 
 # Create the roles DataFrame
@@ -103,30 +103,30 @@ roles = pd.DataFrame({
 roles
 
 
-# In[74]:
+# In[33]:
 
 
 # 2. What is the result of using a right join on the DataFrames?
-users.merge(roles, how='right')
+users.merge(roles, how='right', left_on='role_id', right_on='id')
 
 
-# In[76]:
+# In[35]:
 
 
 # 3. What is the result of using an outer join on the DataFrames?
-users.merge(roles, how='outer')
+users.merge(roles, how='outer', left_on='role_id', right_on='id')
 
 
-# In[231]:
+# In[37]:
 
 
 # 4. What happens if you drop the foreign keys from the DataFrames and try to merge them?
-users_dropped = users.drop(columns='id')
+users_dropped = users.drop(columns='role_id')
 roles_dropped = roles.drop(columns='id')
 users_dropped.merge(roles_dropped, how='outer')
 
 
-# In[93]:
+# In[40]:
 
 
 # 5. Load the mpg dataset from PyDataset.
@@ -134,21 +134,21 @@ mpg = data('mpg')
 mpg
 
 
-# In[94]:
+# In[36]:
 
 
 # 6. Output and read the documentation for the mpg dataset.
-mpg
+data('mpg', show_doc=True)
 
 
-# In[61]:
+# In[41]:
 
 
 # 7. How many rows and columns are in the dataset?
-#234 rows, 11 columns
+mpg.shape
 
 
-# In[97]:
+# In[47]:
 
 
 # 8. Check out your column names and perform any cleanup you may want on them.
@@ -163,59 +163,59 @@ mpg
 mpg.info(), mpg.describe()
 
 
-# In[103]:
+# In[43]:
 
 
 # 10. How many different manufacturers are there?
-mpg.manufacturer.unique(), len(mpg.manufacturer.unique())
+mpg['manufacturer'].nunique()
 
 
-# In[106]:
+# In[45]:
 
 
 # 11. How many different models are there?
-mpg.model.unique(), len(mpg.model.unique())
+mpg['model'].nunique()
 
 
-# In[109]:
+# In[60]:
 
 
 # 12. Create a column named mileage_difference like you did in the DataFrames exercises; this column should contain the difference between highway and city mileage for each car.
-mpg = mpg.assign(mileage_dif = (mpg.highway-mpg.city))
+mpg['mileage_diff'] = mpg['highway'] - mpg['city']
 mpg
 
 
-# In[110]:
+# In[49]:
 
 
 # 13. Create a column named average_mileage like you did in the DataFrames exercises; this is the mean of the city and highway mileage.
-mpg = mpg.assign(avg_mileage = round(((mpg.highway+mpg.city)/2), 2))
+mpg = mpg.assign(avg_mileage = round(((mpg['highway']+mpg['city'])/2), 2))
 mpg
 
 
-# In[111]:
+# In[50]:
 
 
 # 14. Create a new column on the mpg dataset named is_automatic that holds boolean values denoting whether the car has an automatic transmission.
-mpg = mpg.assign(is_auto = mpg.trans.str.contains('auto'))
+mpg = mpg.assign(is_auto = mpg['trans'].str.contains('auto'))
 mpg
 
 
-# In[117]:
+# In[73]:
 
 
 # 15. Using the mpg dataset, find out which which manufacturer has the best miles per gallon on average?
-mpg.groupby('manufacturer').avg_mileage.agg(['mean'])
+mpg.groupby('manufacturer')['avg_mileage'].mean().sort_values(ascending=False)
 
 
-# In[125]:
+# In[72]:
 
 
 # 16. Do automatic or manual cars have better miles per gallon?
-mpg.groupby('is_auto').avg_mileage.agg(['mean']).rename(index={0:'Manual', 1:'Automatic'})
+mpg.groupby('is_auto')['avg_mileage'].mean().rename(index={0:'Manual', 1:'Automatic'})
 
 
-# In[170]:
+# In[81]:
 
 
 # 1. Use your get_db_url function to help you explore the data from the chipotle database.
@@ -224,57 +224,50 @@ chipotle = pd.read_sql('SELECT * FROM orders', chipotle_url)
 chipotle
 
 
-# In[171]:
+# In[82]:
 
 
 # 2. What is the total price for each order?
-chipotle['item_price'] = chipotle['item_price'].str.replace('$','')
+chipotle['item_price'] = chipotle['item_price'].str.replace('$','').astype('float')
 chipotle
 
 
-# In[172]:
+# In[84]:
 
 
-chipotle = chipotle.astype({'item_price':float})
-chipotle
+chipotle.groupby('order_id')['item_price'].sum()
 
 
-# In[173]:
-
-
-chipotle.groupby('order_id').item_price.sum()
-
-
-# In[189]:
+# In[85]:
 
 
 # 3. What are the most popular 3 items?
-chipotle.groupby('item_name').quantity.sum().sort_values(ascending=False).head()
+chipotle.groupby('item_name')['quantity'].sum().nlargest(3)
 
 
-# In[188]:
+# In[86]:
 
 
 # 4. Which item has produced the most revenue?
-chipotle.groupby('item_name').item_price.sum().sort_values(ascending=False).head()
+chipotle.groupby('item_name')['item_price'].sum().nlargest(1)
 
 
-# In[204]:
+# In[90]:
 
 
 # 5. Join the employees and titles DataFrames together.
-emp_title = employees.merge(titles, how='inner', on='emp_no')
+emp_title = employees.merge(titles, on='emp_no')
 emp_title
 
 
-# In[208]:
+# In[91]:
 
 
 # 6. For each title, find the hire date of the employee that was hired most recently with that title.
-emp_title.groupby(['title']).hire_date.max()
+emp_title.groupby('title')['hire_date'].max()
 
 
-# In[229]:
+# In[93]:
 
 
 # 7. Write the code necessary to create a cross tabulation of the number of titles by department. 
@@ -285,13 +278,13 @@ dept_emp = pd.read_sql('SELECT * FROM dept_emp', url)
 departments = pd.read_sql('SELECT * FROM departments', url)
 
 
-# In[230]:
+# In[95]:
 
 
 emp_title_dept = emp_titles.merge(dept_emp, how='inner', on='emp_no')
 full_db = emp_title_dept.merge(departments, how='inner', on='dept_no')
 
-pd.crosstab(full_db.title, full_db.dept_name)
+pd.crosstab(full_db.title, full_db.dept_name).T
 
 
 # In[ ]:
